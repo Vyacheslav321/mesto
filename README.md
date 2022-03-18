f978d289e5b64e7a643555e2d6707d6e8c4eb587
Vyacheslav321 <apxsale@yandex.ru>
2022-03-18 05:40:30 +0300

    добавить заполнение формы

--- README.md
+++ README.md
@@ -3,12 +3,15 @@
 Данный репозиторий создан в рамках обучения на [Yandex.Practicum](https://practicum.yandex.ru "Я практикум")
 
 ### Обзор
-Работа сделана по проекту в [Figma](https://www.figma.com/file/2cn9N9jSkmxD84oJik7xL7/JavaScript.-Sprint-4?node-id=0%3A1)  
+
+Работа сделана по проекту в [Figma](https://www.figma.com/file/2cn9N9jSkmxD84oJik7xL7/JavaScript.-Sprint-4?node-id=0%3A1)
 
 **В работе использованы** технологии FlexBox и Grid. Так же использовались медиа-запросы и псевдоклассы. Адаптивная верстка выполнена для базоых разрешений 320px и 1280px.Верстка выполнена по методологии БЭМ.
 
-****
+---
+
 Cсылка на работу [Mesto-Russia](https://vyacheslav321.github.io/Mesto-Russia/index.html)
 
 #### Моя Статистика :
+
 [![Top Langs](https://github-readme-stats.vercel.app/api/top-langs/?username=Vyacheslav321&layout=compact&theme=vision-friendly)](https://github.com/Vyacheslav321/github-readme-stats)
---
+++ scripts/script.js
@@ -0,0 +1,36 @@
+const popupElement = document.querySelector(".popup");
+const profileEdit = document.querySelector(".profile__button_edit");
+const profileEditClose = document.querySelector(".popup__close-button");
+const ptofileEditSave = document.querySelector(".popup__save-button");
+
+const ESC_KEY = "Escape";
+
+let profileName = document.querySelector(".profile__name");
+let profileWork = document.querySelector(".profile__work");
+let popupName = document.querySelector(".popup__input_name");
+let popupWork = document.querySelector(".popup__input_work");
+
+function openPopup() {
+  popupElement.classList.toggle("hidden");
+  document.addEventListener("keyup", onDocumentKeyUp);
+}
+function closePopup() {
+  popupElement.classList.toggle("hidden");
+  document.removeEventListener("keyup", onDocumentKeyUp);
+}
+
+function closeAndSave() {
+  profileName.textContent = popupName.value;
+  profileWork.textContent = popupWork.value;
+  closePopup();
+}
+
+function onDocumentKeyUp(event) {
+  if (event.key === ESC_KEY) {
+    closePopup();
+  }
+}
+
+profileEdit.addEventListener("click", openPopup);
+profileEditClose.addEventListener("click", closePopup);
+ptofileEditSave.addEventListener("click", closeAndSave);

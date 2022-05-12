d85c15c186ec7647c9426f4b8dfb8264bcbe1b20
Vyacheslav321 <apxsale@yandex.ru>
2022-05-12 21:33:47 +0300

    7 правки 1 итерации проверки

--- scripts/card.js
+++ scripts/card.js
@@ -1,28 +1,51 @@
-//массив карточек по умолчанию
-const initialCards = [
-  {
-    name: "Архыз",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
-  },
-  {
-    name: "Челябинская область",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
-  },
-  {
-    name: "Иваново",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
-  },
-  {
-    name: "Камчатка",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
-  },
-  {
-    name: "Холмогорский район",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
-  },
-  {
-    name: "Байкал",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
-  },
-];
+import { handlePopupImage } from './utils.js';  //импорт функции просмотра фото
 
+export class Card {
+  constructor(picName, picUrl, cardTemplate) {
+      this._picName = picName;
+      this._picUrl = picUrl;
+      this._cardTemplate = cardTemplate;
+      this._handlePopupImage = handlePopupImage;
+  }
+
+  _getTemplate() {
+    const cardElement = document.querySelector(this._cardTemplate).content.querySelector('.element').cloneNode(true);
+    return cardElement;
+  }
+
+  _setEventListeners() {
+    this._elementPic.addEventListener('click', () => {
+      this._handleImageClick(this._picName, this._picUrl);
+    });
+    this._element.querySelector('.element__trash').addEventListener('click', () => {
+      this._handleDeleteClick();
+    });
+    this._elementLike.addEventListener('click', () => {
+      this._handleLikeClick();
+    });
+  }
+
+ _handleImageClick() {
+    this._handlePopupImage(this._picName, this._picUrl);
+  }
+
+  _handleDeleteClick() {
+    this._element.remove();
+    this._element = null;  //при удалении карточки очистить ссылку на DOM-элемент
+  }
+
+  _handleLikeClick() {
+    this._elementLike.classList.toggle("element__like_active");
+  }
+
+  generateCard() {
+    this._element = this._getTemplate();
+    this._elementPic = this._element.querySelector('.element__pic');
+    this._elementLike = this._element.querySelector('.element__like');
+    this._setEventListeners();
+    this._elementPic.src = this._picUrl;
+    this._elementPic.alt = this._picName;
+    this._element.querySelector(".element__text").textContent = this._picName;
+    return this._element;
+  }
+};

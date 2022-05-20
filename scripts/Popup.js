7511eefb5d891797a469845c1a603ddf99ed81a6
Vyacheslav321 <apxsale@yandex.ru>
2022-05-20 17:14:33 +0300

    добавить шаблоны классов

---
+++ scripts/Popup.js
@@ -0,0 +1,15 @@
+class Popup {
+  constructor(popupSelector) {}
+  open() {  //открытие попапа
+
+  };
+  close() { //закрытие попапа
+    
+  };
+  _handleEscClose() { //закрытие попапа по esc
+
+  };
+  setEventListeners() { //слушатель клика по крестику и серой области
+
+  };
+}
---
+++ scripts/PopupWithImage.js
@@ -0,0 +1,10 @@
+//Для каждого попапа создавайте свой экземпляр класса PopupWithForm
+
+class PopupWithForm {
+  constructor (Popup) {
+    super.Popup
+  };
+  open() {  //здесь вставлять в попап картинку с src изображения и подписью к картинке
+
+  };
+}
